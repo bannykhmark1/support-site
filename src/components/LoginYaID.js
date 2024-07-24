1c5135cce5a27359a1a05b59378ddceedec57342
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-24 12:48:16 +0500

    asdfasdfasdfasdf

--- src/components/LoginYaID.js
+++ src/components/LoginYaID.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LoginYaID = () => {
+const LoginYaID = ({ setIsYandexAuth }) => {
   const redirectToYandex = () => {
     const redirectUri = encodeURIComponent('https://support.hobbs-it.ru/');
     const yandexAuthUrl = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${process.env.REACT_APP_YANDEX_CLIENT_ID}&redirect_uri=${redirectUri}`;
@@ -9,6 +9,7 @@ const LoginYaID = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+       {console.log(isYandexAuth)}
       <button
         onClick={redirectToYandex}
         className="flex items-center px-4 py-2 bg-yellow-400 hover:bg-yellow-500 text-black font-semibold rounded-lg shadow-md transition-colors duration-300"
