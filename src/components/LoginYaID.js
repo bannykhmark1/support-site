b0f5f135d3b6c0db36dd5ae4e9fda343fdfac909
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-24 13:18:47 +0500

    asfasdfg

--- src/components/LoginYaID.js
+++ src/components/LoginYaID.js
@@ -9,7 +9,7 @@ const LoginYaID = ({ setIsYandexAuth }) => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-       {console.log(isYandexAuth)}
+
       <button
         onClick={redirectToYandex}
         className="flex items-center px-4 py-2 bg-yellow-400 hover:bg-yellow-500 text-black font-semibold rounded-lg shadow-md transition-colors duration-300"
