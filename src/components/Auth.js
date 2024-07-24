ddebcb12e2a3adcbc270f5a3baabfbb21f242182
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-24 13:49:41 +0500

    optimization

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,19 +1,14 @@
 import React, { useContext, useState } from 'react';
-import { useLocation, useNavigate } from "react-router-dom";
-import { LOGIN_ROUTE, REGISTRATION_ROUTE, APP_ROUTE, FORGOT_PASSWORD_ROUTE, RESET_PASSWORD_ROUTE } from "../utils/consts";
-import { login, registration } from "../http/userAPI";
-import { observer } from "mobx-react-lite";
-import { Context } from "../index";
+import { useNavigate } from 'react-router-dom';
+import { LOGIN_ROUTE, APP_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
+import { login, registration } from '../http/userAPI';
+import { observer } from 'mobx-react-lite';
+import { Context } from '../index';
 import { NavLink } from 'react-router-dom';
-import ForgotPassword from '../components/ForgotPassword';
-import ResetPassword from '../components/ResetPassword';
 
 const Auth = observer(() => {
     const { user } = useContext(Context);
-    const location = useLocation();
     const navigate = useNavigate();
-    const isForgotPassword = location.pathname === FORGOT_PASSWORD_ROUTE;
-    const isResetPassword = location.pathname === RESET_PASSWORD_ROUTE;
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
@@ -46,75 +41,63 @@ const Auth = observer(() => {
     };
 
     return (
-        <div className='flex flex-col justify-between '>
+        <div className='flex flex-col justify-between h-screen'>
             <div className="flex items-center justify-center h-full">
                 <div className="w-full max-w-md p-8 bg-white rounded shadow-lg">
-                    {isForgotPassword ? (
-                        <ForgotPassword />
-                    ) : isResetPassword ? (
-                        <ResetPassword />
-                    ) : (
-                        <>
-                            <h2 className="text-2xl font-bold text-center">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
-                            <div className="mt-6 space-y-4">
-                                {!isLogin && (
-                                    <input
-                                        className="w-full px-3 py-2 border rounded"
-                                        placeholder="Введите ваше имя..."
-                                        value={name}
-                                        onChange={e => setName(e.target.value)}
-                                    />
-                                )}
-                                <input
-                                    className="w-full px-3 py-2 border rounded"
-                                    placeholder="Введите ваш email..."
-                                    value={email}
-                                    onChange={e => setEmail(e.target.value)}
-                                />
-                                <input
-                                    className="w-full px-3 py-2 border rounded"
-                                    placeholder="Введите ваш пароль..."
-                                    value={password}
-                                    onChange={e => setPassword(e.target.value)}
-                                    type="password"
-                                />
-                                <div className="flex items-center justify-between mt-4">
-                                    {isLogin ? (
-                                        <div className="flex items-center space-x-2">
-                                            <span>Нет аккаунта?</span>
-                                            <button
-                                                className="text-blue-600 hover:underline"
-                                                onClick={() => setIsLogin(false)}
-                                            >
-                                                Зарегистрируйся!
-                                            </button>
-                                        </div>
-                                    ) : (
-                                        <div className="flex items-center space-x-2">
-                                            <span>Есть аккаунт?</span>
-                                            <button
-                                                className="text-blue-600 hover:underline"
-                                                onClick={() => setIsLogin(true)}
-                                            >
-                                                Войдите!
-                                            </button>
-                                        </div>
-                                    )}
+                    <h2 className="text-2xl font-bold text-center">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+                    <div className="mt-6 space-y-4">
+                        {!isLogin && (
+                            <input
+                                className="w-full px-3 py-2 border rounded"
+                                placeholder="Введите ваше имя..."
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                            />
+                        )}
+                        <input
+                            className="w-full px-3 py-2 border rounded"
+                            placeholder="Введите ваш email..."
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                        <input
+                            className="w-full px-3 py-2 border rounded"
+                            placeholder="Введите ваш пароль..."
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            type="password"
+                        />
+                        <div className="flex items-center justify-between mt-4">
+                            {isLogin ? (
+                                <div className="flex items-center space-x-2">
+                                    <span>Нет аккаунта?</span>
                                     <button
-                                        className="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600"
-                                        onClick={click}
+                                        className="text-blue-600 hover:underline"
+                                        onClick={() => setIsLogin(false)}
                                     >
-                                        {isLogin ? 'Войти' : 'Регистрация'}
+                                        Зарегистрируйся!
                                     </button>
                                 </div>
-                                {isLogin && (
-                                    <div className="flex items-center justify-end mt-2">
-                                        <NavLink to={FORGOT_PASSWORD_ROUTE} className="text-blue-600 hover:underline">Забыли пароль?</NavLink>
-                                    </div>
-                                )}
-                            </div>
-                        </>
-                    )}
+                            ) : (
+                                <div className="flex items-center space-x-2">
+                                    <span>Есть аккаунт?</span>
+                                    <button
+                                        className="text-blue-600 hover:underline"
+                                        onClick={() => setIsLogin(true)}
+                                    >
+                                        Войдите!
+                                    </button>
+                                </div>
+                            )}
+                            <button
+                                className="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600"
+                                onClick={click}
+                            >
+                                {isLogin ? 'Войти' : 'Регистрация'}
+                            </button>
+                        </div>
+     
+                    </div>
                 </div>
             </div>
         </div>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import { LOGIN_ROUTE, REGISTRATION_ROUTE, APP_ROUTE, FORGOT_PASSWORD_ROUTE, RESET_PASSWORD_ROUTE, ANNOUNCEMENTS_ROUTE, CREATE_ANNOUNCEMENT_ROUTE, EDIT_ANNOUNCEMENT_ROUTE } from "./utils/consts";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE, APP_ROUTE, ANNOUNCEMENTS_ROUTE, CREATE_ANNOUNCEMENT_ROUTE, EDIT_ANNOUNCEMENT_ROUTE } from "./utils/consts";
 import Auth from "./components/Auth";
 import App from "./App";
-import ForgotPassword from "./components/ForgotPassword";
-import ResetPassword from "./components/ResetPassword";
+
 import CreateAnnouncement from "./components/CreateAnnouncement";
 import ListAnnouncement from "./components/ListAnnouncement";
 import EditAnnouncement from "./components/EditAnnouncement";
@@ -33,14 +32,7 @@ export const publicRoutes = [
     path: REGISTRATION_ROUTE,
     element: <Auth />
   },
-  {
-    path: FORGOT_PASSWORD_ROUTE,
-    element: <ForgotPassword />
-  },
-  {
-    path: RESET_PASSWORD_ROUTE,
-    element: <ResetPassword />
-  },
+
   {
     path: ANNOUNCEMENTS_ROUTE,
     element: <ListAnnouncement />
