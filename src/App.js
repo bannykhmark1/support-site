364e9bc25d0d3c88f785df730817915e519be3bc
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-25 09:48:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Context } from "./index";
+import { check } from "./http/userAPI";
+import Header from './components/Header';
+import ContactForm from './components/ContactForm';
+import ListAnnouncement from './components/ListAnnouncement';
+import MessengerWidget from './components/MessengerWidget';
+import LoginYaID from './components/LoginYaID';
+import './App.css';
 import axios from 'axios';
 
-const App = () => {
-  const [isAuth, setIsAuth] = useState(false);
+function App() {
+  const { user } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+  const [isYandexAuth, setIsYandexAuth] = useState(false);
 
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
     const code = queryParams.get('code');
-    
     if (code) {
-      // Отправляем код на сервер для обмена на токен
       axios.post('/api/auth/yandex', { code })
         .then(response => {
-          // Обработка успешного ответа
-          console.log('Данные пользователя:', response.data);
-          setIsAuth(true);
-          // Сохраните токен в localStorage или в контексте, если это необходимо
+          const userData = response.data;
+          user.setUser(userData);
+          user.setIsAuth(true);
+          setIsYandexAuth(true);
         })
         .catch(error => {
           console.error('Ошибка при отправке кода на сервер:', error);
-        });
+          setIsYandexAuth(false);
+        })
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
     }
-  }, []);
+  }, [user]);
+
+  useEffect(() => {
+    if (isYandexAuth) {
+      check()
+        .then(userData => {
+          if (userData) {
+            user.setUser(userData);
+          }
+        })
+        .catch(err => {
+          console.error('Ошибка при проверке пользователя', err);
+        })
+        .finally(() => setLoading(false));
+    }
+  }, [isYandexAuth, user]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading...
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <h1>Мой сайт</h1>
-      {isAuth ? (
-        <p>Добро пожаловать!</p>
-      ) : (
-        <LoginYaID />
-      )}
+    <div className="bg-gray-100 min-h-screen p-4">
+      <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-lg">
+        <Header />
+        {isYandexAuth ? (
+          <>
+            <MessengerWidget />
+            <ListAnnouncement />
+            <ContactForm />
+          </>
+        ) : (
+          <LoginYaID />
+        )}
+      </div>
     </div>
   );
-};
+}
 
 export default App;
