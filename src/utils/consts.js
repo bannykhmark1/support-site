ffce1ef24a63119163dd695e9ee34f03d0a03ee6
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-25 15:38:46 +0500

    Update consts.js

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -6,3 +6,4 @@ export const RESET_PASSWORD_ROUTE = '/resetPassword/:token';
 export const ANNOUNCEMENTS_ROUTE = '/announcements';
 export const CREATE_ANNOUNCEMENT_ROUTE = '/announcements/create';
 export const EDIT_ANNOUNCEMENT_ROUTE = '/announcements/edit/:id';
+export const REDIRECT_TOKEN = '/redirect'
