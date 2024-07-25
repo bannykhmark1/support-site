f27421caea48510bdee6101d9e994038f0375a86
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-25 09:43:22 +0500

    Update LoginYaID.js

--- src/components/LoginYaID.js
+++ src/components/LoginYaID.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
 const LoginYaID = ({ setIsYandexAuth }) => {
-const redirectToYandex = () => {
-  const redirectUri = encodeURIComponent('https://support.hobbs-it.ru/');
-  const yandexAuthUrl = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${process.env.REACT_APP_YANDEX_CLIENT_ID}&redirect_uri=${redirectUri}`;
-  console.log('Нажата кнопка авторизации через Яндекс');
-  console.log('URL для авторизации через Яндекс:', yandexAuthUrl); 
-  window.location.href = yandexAuthUrl;
-};
-
+  const redirectToYandex = () => {
+    const redirectUri = encodeURIComponent('https://support.hobbs-it.ru/');
+    const yandexAuthUrl = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${process.env.REACT_APP_YANDEX_CLIENT_ID}&redirect_uri=${redirectUri}`;
+    window.location.href = yandexAuthUrl;
+  };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
 
