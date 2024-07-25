d71cffa6cc9c4a9f13da6d7f1a40bf4f7d5f7fc1
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-25 15:36:02 +0500

    Create RedirectToken.js

---
+++ src/components/RedirectToken.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const RedirectToken = ({  }) => {
+    YaSendSuggestToken(
+        'https://support.hobbs-it.ru/', 
+        {
+           flag: true
+        }
+     )
+
+  return <div>
+
+  </div>;
+};
+
+export default RedirectToken;
