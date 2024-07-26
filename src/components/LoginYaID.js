dbb66efe604b7035a08944a2b3a3b945bf8abb6c
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-26 14:11:15 +0500

    фывфыв

--- src/components/LoginYaID.js
+++ src/components/LoginYaID.js
@@ -2,58 +2,32 @@ import React, { useEffect } from 'react';
 
 function LoginYaID({ onAuthSuccess }) {
   useEffect(() => {
-    const initializeYandexAuth = async () => {
-      if (window.YaAuthSuggest && !window.yandexAuthInitialized) {
-        try {
-          const { handler } = await window.YaAuthSuggest.init(
-            {
-              client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
-              response_type: 'token',
-              redirect_uri: 'https://support.hobbs-it.ru/'
-            },
-            'https://support.hobbs-it.ru/redirect',
-            {
-              view: 'button',
-              parentId: 'container',
-              buttonView: 'main',
-              buttonTheme: 'light',
-              buttonSize: 'm',
-              buttonBorderRadius: 0
-            }
-          );
-
-          window.yandexAuthHandler = handler;
-          window.yandexAuthInitialized = true;
-        } catch (error) {
-          if (error.code !== 'in_progress') {
-            console.log('Ошибка при инициализации Яндекс авторизации:', error);
-          }
+    if (window.YaAuthSuggest) {
+      window.YaAuthSuggest.init(
+        {
+          client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
+          response_type: 'token',
+          redirect_uri: 'https://support.hobbs-it.ru/'
+        },
+        'https://support.hobbs-it.ru/redirect',
+        {
+          view: 'button',
+          parentId: 'container',
+          buttonView: 'main',
+          buttonTheme: 'light',
+          buttonSize: 'm',
+          buttonBorderRadius: 0
         }
-      }
-    };
-
-    initializeYandexAuth();
+      )
+      .then(({ handler }) => handler())
+      .then(data => {
+        onAuthSuccess(data); // Передаем данные в App
+      })
+    }
   }, [onAuthSuccess]);
 
-  const openAuthWindow = () => {
-    if (window.yandexAuthHandler) {
-      const authWindow = window.open(
-        '',
-        'YandexAuth',
-        'width=500,height=600'
-      );
-      window.yandexAuthHandler(() => {
-        authWindow.location.href = 'https://oauth.yandex.ru/authorize?response_type=token&client_id=' + process.env.REACT_APP_YANDEX_CLIENT_ID + '&redirect_uri=https://support.hobbs-it.ru/redirect';
-      });
-    }
-  };
-
   return (
-    <div id="container">
-      <button onClick={openAuthWindow} className="bg-blue-500 text-white px-4 py-2 rounded">
-        Войти с Яндекс ID
-      </button>
-    </div>
+    <div id="container"></div>
   );
 }
 
