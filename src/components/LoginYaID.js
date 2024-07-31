1f02bf6696bd2c109e9e75039f2d880fb1c57cb1
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-31 10:09:20 +0500

    Update LoginYaID.js

--- src/components/LoginYaID.js
+++ src/components/LoginYaID.js
@@ -1,38 +1,32 @@
-import React, { useEffect, useCallback } from 'react';
+  import React, { useEffect } from 'react';
 
-function LoginYaID({ onAuthSuccess }) {
-  const initYaAuthSuggest = useCallback(() => {
-    if (window.YaAuthSuggest) {
-      window.YaAuthSuggest.init(
-        {
-          client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
-          response_type: 'token',
-          redirect_uri: 'https://support.hobbs-it.ru/redirect',
-        },
-        'https://support.hobbs-it.ru/',
-      )
-      .then(({ handler }) => handler())
-      .then(data => {
-        onAuthSuccess(data); // Передаем данные в App
-      })
-      .catch(error => {
-        if (error.code !== 'in_progress') {
-          console.error('Ошибка при инициализации Яндекс авторизации:', error);
-        }
-      });
-    }
-  }, [onAuthSuccess]);
+  function LoginYaID({ onAuthSuccess }) {
+    useEffect(() => {
+      if (window.YaAuthSuggest) {
+        window.YaAuthSuggest.init(
+          {
+            client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
+            response_type: 'token',
+            redirect_uri: 'https://support.hobbs-it.ru/redirect',
+          },
+          'https://support.hobbs-it.ru/',
+ф
+        )
+        .then(({ handler }) => handler())
+        .then(data => {
+          onAuthSuccess(data); // Передаем данные в App
+        })
+        .catch(error => {
+          if (error.code !== 'in_progress') {
+            console.error('Ошибка при инициализации Яндекс авторизации:', error);
+          }
+        });
+      }
+    }, [onAuthSuccess]);
 
-  useEffect(() => {
-    initYaAuthSuggest();
-  }, [initYaAuthSuggest]);
-
-  return (
-    <div>
+    return (
       <div id="container"></div>
-      <button onClick={initYaAuthSuggest}>Открыть авторизацию Яндекс</button>
-    </div>
-  );
-}
+    );
+  }
 
-export default LoginYaID;
+  export default LoginYaID;
