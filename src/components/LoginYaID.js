f64aef645f65cade03c20aeb595f39f46f084f1e
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-31 10:20:47 +0500

    Update LoginYaID.js

--- src/components/LoginYaID.js
+++ src/components/LoginYaID.js
@@ -1,31 +1,29 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 
 function LoginYaID({ onAuthSuccess }) {
-  const initYaAuthSuggest = useCallback(() => {
-
-      window.YaAuthSuggest.init(
-        {
-          client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
-          response_type: 'token',
-          redirect_uri: 'https://support.hobbs-it.ru/redirect',
-        },
-        'https://support.hobbs-it.ru/',
-      )
-      .then(({ handler }) => handler())
-      .then(data => {
-        onAuthSuccess(data); // Передаем данные в App
-      })
-      .catch(error => {
-        if (error.code !== 'in_progress') {
-          console.error('Ошибка при инициализации Яндекс авторизации:', error);
-        }
-      });
-    
-  }, [onAuthSuccess]);
+  const initYaAuthSuggest = () => {
+    window.YaAuthSuggest.init(
+      {
+        client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
+        response_type: 'token',
+        redirect_uri: 'https://support.hobbs-it.ru/redirect',
+      },
+      'https://support.hobbs-it.ru/',
+    )
+    .then(({ handler }) => handler())
+    .then(data => {
+      onAuthSuccess(data); // Передаем данные в App
+    })
+    .catch(error => {
+      if (error.code !== 'in_progress') {
+        console.error('Ошибка при инициализации Яндекс авторизации:', error);
+      }
+    });
+  };
 
   useEffect(() => {
     initYaAuthSuggest();
-  }, [initYaAuthSuggest]);
+  }, [onAuthSuccess]);
 
   return (
     <div>
