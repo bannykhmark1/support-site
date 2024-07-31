6c9f8943da5d2f0b806b7ed88d29e24b0e661e41
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-31 10:17:48 +0500

    Update LoginYaID.js

--- src/components/LoginYaID.js
+++ src/components/LoginYaID.js
@@ -1,31 +1,38 @@
-  import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 
-  function LoginYaID({ onAuthSuccess }) {
-    useEffect(() => {
-      if (window.YaAuthSuggest) {
-        window.YaAuthSuggest.init(
-          {
-            client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
-            response_type: 'token',
-            redirect_uri: 'https://support.hobbs-it.ru/redirect',
-          },
-          'https://support.hobbs-it.ru/',
-        )
-        .then(({ handler }) => handler())
-        .then(data => {
-          onAuthSuccess(data); // Передаем данные в App
-        })
-        .catch(error => {
-          if (error.code !== 'in_progress') {
-            console.error('Ошибка при инициализации Яндекс авторизации:', error);
-          }
-        });
-      }
-    }, [onAuthSuccess]);
+function LoginYaID({ onAuthSuccess }) {
+  const initYaAuthSuggest = useCallback(() => {
+    if (window.YaAuthSuggest) {
+      window.YaAuthSuggest.init(
+        {
+          client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
+          response_type: 'token',
+          redirect_uri: 'https://support.hobbs-it.ru/redirect',
+        },
+        'https://support.hobbs-it.ru/',
+      )
+      .then(({ handler }) => handler())
+      .then(data => {
+        onAuthSuccess(data); // Передаем данные в App
+      })
+      .catch(error => {
+        if (error.code !== 'in_progress') {
+          console.error('Ошибка при инициализации Яндекс авторизации:', error);
+        }
+      });
+    }
+  }, [onAuthSuccess]);
 
-    return (
+  useEffect(() => {
+    initYaAuthSuggest();
+  }, [initYaAuthSuggest]);
+
+  return (
+    <div>
       <div id="container"></div>
-    );
-  }
+      <button onClick={initYaAuthSuggest}>Открыть авторизацию Яндекс</button>
+    </div>
+  );
+}
 
-  export default LoginYaID;
+export default LoginYaID;
