fe0fcf868af3cf3fed2f748363858534ffd4ceca
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-08-09 11:44:50 +0500

    фывфыв

--- src/components/LoginYaID.js
+++ src/components/LoginYaID.js
@@ -1,39 +1,42 @@
-  import React, { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
-  function LoginYaID({ onAuthSuccess }) {
-    useEffect(() => {
-      if (window.YaAuthSuggest) {
-        window.YaAuthSuggest.init(
-          {
-            client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
-            response_type: 'token',
-            redirect_uri: 'https://support.hobbs-it.ru/redirect',
-          },
-          'https://support.hobbs-it.ru/',
-          {
-            view: 'button',
-            parentId: 'container',
-            buttonView: 'main',
-            buttonTheme: 'light',
-            buttonSize: 'm',
-            buttonBorderRadius: 0,
-          }
-        )
-        .then(({ handler }) => handler())
-        .then(data => {
-          onAuthSuccess(data); // Передаем данные в App
-        })
-        .catch(error => {
-          if (error.code !== 'in_progress') {
-            console.error('Ошибка при инициализации Яндекс авторизации:', error);
-          }
-        });
-      }
-    }, [onAuthSuccess]);
+function LoginYaID({ onAuthSuccess }) {
+  useEffect(() => {
+    if (window.YaAuthSuggest) {
+      window.YaAuthSuggest.init(
+        {
+          client_id: process.env.REACT_APP_YANDEX_CLIENT_ID,
+          response_type: 'token',
+          redirect_uri: 'https://support.hobbs-it.ru/redirect',
+        },
+        'https://support.hobbs-it.ru/',
+        {
+          view: 'button',
+          parentId: 'container',
+          buttonView: 'main',
+          buttonTheme: 'light',
+          buttonSize: 'm',
+          buttonBorderRadius: 0,
+        }
+      )
+      .then(({ handler }) => {
+        return handler();
+      })
+      .then(data => {
+        console.log('Auth data:', data);
+        onAuthSuccess(data); // Передаем данные в App
+      })
+      .catch(error => {
+        if (error.code !== 'in_progress') {
+          console.error('Ошибка при инициализации Яндекс авторизации:', JSON.stringify(error, null, 2));
+        }
+      });
+    }
+  }, [onAuthSuccess]);
 
-    return (
-      <div id="container"></div>
-    );
-  }
+  return (
+    <div id="container"></div>
+  );
+}
 
-  export default LoginYaID;
+export default LoginYaID;
