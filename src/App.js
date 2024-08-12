1bfe616b0cd23f7d301151078d5922249052b96d
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-08-12 12:18:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Feedback from "./components/FeedBack";
 import "./App.css";
 
 function App() {
-  const [isYandexAuth, setIsYandexAuth] = useState(true);
+  const [isYandexAuth, setIsYandexAuth] = useState(false);
   const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
 
   useEffect(() => {
