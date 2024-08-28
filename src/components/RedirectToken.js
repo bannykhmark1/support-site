5557d4054a16ac8c20b5419c0709c34230ae4489
bannykhmark1 <bannykhmark@yandex.ru>
2024-08-28 09:45:26 +0500

    фыв

--- src/components/RedirectToken.js
+++ src/components/RedirectToken.js
@@ -4,8 +4,10 @@ const RedirectToken = ({ onAuthSuccess }) => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.hash.slice(1));
     const token = params.get('access_token');
-    if (token) {
+    if (token && typeof onAuthSuccess === 'function') {
       onAuthSuccess({ access_token: token });
+    } else {
+      console.error("onAuthSuccess is not a function or token is missing.");
     }
   }, [onAuthSuccess]);
 
