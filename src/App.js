a70c1f5960e24291b6dc8a0f4a831d724965efd3
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-08-29 13:13:46 +0500

    фывфыв

--- src/App.js
+++ src/App.js
@@ -5,35 +5,35 @@ import ListAnnouncement from "./components/ListAnnouncement";
 import Feedback from "./components/FeedBack";
 import "./App.css";
 import Auth from "./components/Auth";
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userRole, setUserRole] = useState(null); // Для хранения роли пользователя
+  const [userRole, setUserRole] = useState(null);
 
   const handleLogin = () => {
     // Логика для авторизации пользователя
     setIsAuthenticated(true);
-    // Установка роли пользователя после успешной авторизации
-    setUserRole("user"); // Например, "admin" или "user"
+    setUserRole("user"); // Устанавливаем роль пользователя
   };
 
   const handleLogout = () => {
     setIsAuthenticated(false);
-    setUserRole(null); // Сброс роли пользователя
+    setUserRole(null);
     window.location.reload(); // Перезагружаем страницу после выхода
   };
 
   return (
     <div className="bg-gray-100 min-h-screen p-4">
       <div className="max-w-6xl mx-auto bg-white p-6 rounded-lg shadow-lg">
-        <Auth />
-        <Header isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
+        {/* Если пользователь не авторизован, показываем компонент Auth */}
         {!isAuthenticated ? (
-          <button onClick={handleLogin}>Login</button>
+          <Auth onLogin={handleLogin} />
         ) : (
           <>
+            <Header isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
             <div className="md:flex">
               <ContactForm />
-              <ListAnnouncement userRole={userRole} /> {/* Передаем роль пользователя */}
+              <ListAnnouncement userRole={userRole} />
             </div>
             <Feedback />
           </>
