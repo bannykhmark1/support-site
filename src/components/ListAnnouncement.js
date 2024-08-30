c94ecc84e668259be06ec633c84c9b6db3fbbfdf
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-08-30 13:33:19 +0500

    asdasd

--- src/components/ListAnnouncement.js
+++ src/components/ListAnnouncement.js
@@ -42,8 +42,6 @@ const ListAnnouncement = ({ userRole }) => {
     // Проверяем, является ли пользователь администратором
     const isAdmin = userRole === 'ADMIN';
 
-    console.log(isAdmin)
-
     return (
         <div className="max-w-xl mx-auto mt-10 p-4">
             <div className="flex justify-between items-center mb-6">
@@ -64,7 +62,7 @@ const ListAnnouncement = ({ userRole }) => {
                         <div className="text-gray-600 mb-2">Команда поддержки УАГ</div>
                         <h3 className="text-xl font-semibold text-gray-900 mb-4">{announcement.title}</h3>
                         <p className="text-gray-700 mb-4">{announcement.description}</p>
-                        <p className="text-gray-700 text-sm font-bold">{(announcement.date)}</p>
+                        <p className="text-gray-700 text-sm font-bold">{formatDate(announcement.date)}</p>
                         {isAdmin && (
                             <div className="flex justify-end">
                                 <button
