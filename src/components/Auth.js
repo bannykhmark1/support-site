bd065fac5f224c3f8c827fd2baf4313dae5de38f
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-08-30 12:27:26 +0500

    фывфыв

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -51,7 +51,7 @@ const Auth = observer(({ onLogin, setAuthState }) => {
     };
 
     return (
-        <div className='flex flex-col justify-between h-screen'>
+        <div className='flex flex-col justify-between'>
             <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
             <div className="flex items-center justify-center h-full">
                 <div className="w-full max-w-md p-8 bg-white rounded shadow-lg">
