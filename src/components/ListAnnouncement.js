36a6c81143f31e8f857e56437400a26fb3e86dc6
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-08-30 13:31:05 +0500

    asdasd

--- src/components/ListAnnouncement.js
+++ src/components/ListAnnouncement.js
@@ -64,7 +64,7 @@ const ListAnnouncement = ({ userRole }) => {
                         <div className="text-gray-600 mb-2">Команда поддержки УАГ</div>
                         <h3 className="text-xl font-semibold text-gray-900 mb-4">{announcement.title}</h3>
                         <p className="text-gray-700 mb-4">{announcement.description}</p>
-                        <p className="text-gray-700 text-sm font-bold">{(dateString(announcement.date))}</p>
+                        <p className="text-gray-700 text-sm font-bold">{(formatDate(announcement.date))}</p>
                         {isAdmin && (
                             <div className="flex justify-end">
                                 <button
