46faffd8cae11d828851c243feb1ced34e24c3d2
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-10-01 14:22:43 +0500

    asdasd

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 import { Context } from '../index';
-import { sendVerificationCode, verifyCodeAPI, setNewPasswordAPI } from '../http/userAPI';
+import { sendVerificationCode, verifyCodeAPI, setNewPasswordAPI, loginWithPasswordAPI } from '../http/userAPI';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -12,8 +12,10 @@ const Auth = observer(({ onLogin, setAuthState }) => {
     const [email, setEmail] = useState('');
     const [code, setCode] = useState('');
     const [newPassword, setNewPassword] = useState('');
+    const [password, setPassword] = useState(''); // Для входа по паролю
     const [isCodeSent, setIsCodeSent] = useState(false);
     const [isCodeVerified, setIsCodeVerified] = useState(false);
+    const [authMethod, setAuthMethod] = useState('code'); // 'code' или 'password'
 
     const handleSendCode = async () => {
         try {
@@ -49,58 +51,100 @@ const Auth = observer(({ onLogin, setAuthState }) => {
         }
     };
 
+    const handleLogin = async () => {
+        if (authMethod === 'code') {
+            if (isCodeSent) {
+                await handleVerifyCode();
+            } else {
+                await handleSendCode();
+            }
+        } else if (authMethod === 'password') {
+            try {
+                const data = await loginWithPasswordAPI(email, password);
+                if (data.token) {
+                    // Здесь вы можете сохранить токен и обновить состояние аутентификации
+                    toast.success('Успешный вход!');
+                    navigate('/'); // Перенаправление на главную страницу или в нужный раздел
+                }
+            } catch (e) {
+                toast.error(e.response?.data?.message || "Ошибка при входе");
+            }
+        }
+    };
+
     return (
         <div className='flex flex-col justify-between min-h-full'>
             <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
             <div className="flex items-center justify-center h-full">
                 <div className="w-full max-w-md p-8 bg-white rounded shadow-lg">
                     <h2 className="text-2xl font-bold text-center">Авторизация</h2>
+                    <div className="flex justify-center space-x-4 mt-4">
+                        <button 
+                            className={`px-4 py-2 rounded ${authMethod === 'code' ? 'bg-green-500 text-white' : 'bg-gray-200'}`} 
+                            onClick={() => setAuthMethod('code')}
+                        >
+                            Вход по коду
+                        </button>
+                        <button 
+                            className={`px-4 py-2 rounded ${authMethod === 'password' ? 'bg-green-500 text-white' : 'bg-gray-200'}`} 
+                            onClick={() => setAuthMethod('password')}
+                        >
+                            Вход по паролю
+                        </button>
+                    </div>
                     <div className="mt-6 space-y-4">
-                        <input
-                            className="w-full px-3 py-2 border rounded"
-                            placeholder="Введите ваш email..."
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        {isCodeSent && (
-                            <input
-                                className="w-full px-3 py-2 border rounded"
-                                placeholder="Введите код..."
-                                value={code}
-                                onChange={e => setCode(e.target.value)}
-                            />
+                        {authMethod === 'code' && (
+                            <>
+                                <input
+                                    className="w-full px-3 py-2 border rounded"
+                                    placeholder="Введите ваш email..."
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                                {isCodeSent && (
+                                    <input
+                                        className="w-full px-3 py-2 border rounded"
+                                        placeholder="Введите код..."
+                                        value={code}
+                                        onChange={e => setCode(e.target.value)}
+                                    />
+                                )}
+                                {isCodeVerified && (
+                                    <input
+                                        className="w-full px-3 py-2 border rounded"
+                                        placeholder="Введите новый пароль..."
+                                        value={newPassword}
+                                        onChange={e => setNewPassword(e.target.value)}
+                                    />
+                                )}
+                            </>
                         )}
-                        {isCodeVerified && (
-                            <input
-                                className="w-full px-3 py-2 border rounded"
-                                placeholder="Введите новый пароль..."
-                                value={newPassword}
-                                onChange={e => setNewPassword(e.target.value)}
-                            />
+
+                        {authMethod === 'password' && (
+                            <>
+                                <input
+                                    className="w-full px-3 py-2 border rounded"
+                                    placeholder="Введите ваш email..."
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                                <input
+                                    className="w-full px-3 py-2 border rounded"
+                                    placeholder="Введите пароль..."
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    type="password" // Замените на type="password" для безопасности
+                                />
+                            </>
                         )}
+
                         <div className="flex items-center justify-between mt-4">
-                            {!isCodeSent ? (
-                                <button
-                                    className="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600"
-                                    onClick={handleSendCode}
-                                >
-                                    Отправить код
-                                </button>
-                            ) : isCodeVerified ? (
-                                <button
-                                    className="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600"
-                                    onClick={handleChangePassword}
-                                >
-                                    Установить пароль
-                                </button>
-                            ) : (
-                                <button
-                                    className="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600"
-                                    onClick={handleVerifyCode}
-                                >
-                                    Проверить код
-                                </button>
-                            )}
+                            <button
+                                className="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600"
+                                onClick={handleLogin}
+                            >
+                                {authMethod === 'code' ? (isCodeSent ? 'Проверить код' : 'Отправить код') : 'Войти'}
+                            </button>
                         </div>
                     </div>
                 </div>
