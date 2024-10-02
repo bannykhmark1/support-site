adfc5d9623f4d1c790629dcb30acc77e65abe6ea
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-10-02 17:10:24 +0500

    фывфыв

--- src/components/ListAnnouncement.js
+++ src/components/ListAnnouncement.js
@@ -11,7 +11,6 @@ const ListAnnouncement = ({ userRole }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [showAll, setShowAll] = useState(false); // Состояние для показа всех объявлений
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [isViewAllModalOpen, setIsViewAllModalOpen] = useState(false); // Для отображения модального окна со всеми объявлениями
     const [currentAnnouncementId, setCurrentAnnouncementId] = useState(null);
     const [activeMenu, setActiveMenu] = useState(null);
 
@@ -40,7 +39,6 @@ const ListAnnouncement = ({ userRole }) => {
             console.error('Failed to update announcement:', error);
         }
     };
-
 
     const handleDelete = async (id) => {
         try {
@@ -85,7 +83,10 @@ const ListAnnouncement = ({ userRole }) => {
 
             <div className="bg-white w-full shadow-lg rounded-lg p-6 mb-6">
                 {visibleAnnouncements.map((announcement) => (
-                    <div key={announcement.id} className="mb-6 pb-6 border-b border-gray-200 relative">
+                    <div
+                        key={announcement.id}
+                        className={`mb-6 pb-6 border-b border-gray-200 relative ${announcement.isResolved ? 'border-green-500' : 'border-red-500'}`}
+                    >
                         <div className="text-gray-600 mb-2">Команда поддержки УАГ</div>
                         <h3 className="text-xl font-semibold text-gray-900 mb-4">
                             {announcement.title}
@@ -97,19 +98,25 @@ const ListAnnouncement = ({ userRole }) => {
                         <p className="text-gray-400 text-sm">
                             {announcement.date.split('T')[0]}
                             &nbsp;
-                            {announcement.date.slice('11', '19')}
+                            {announcement.date.slice(11, 19)}
                         </p>
-                        <label className="inline-flex items-center">
-                            <input
-                                type="checkbox"
-                                checked={announcement.isResolved}
-                                onChange={() => handleCheckboxChange(announcement.id, announcement.isResolved)}
-                                className={`form-checkbox ${announcement.isResolved ? 'text-green-500' : 'text-red-500'}`}
-                            />
+                        {isAdmin ? (
+                            <label className="inline-flex items-center">
+                                <input
+                                    type="checkbox"
+                                    checked={announcement.isResolved}
+                                    onChange={() => handleCheckboxChange(announcement.id, announcement.isResolved)}
+                                    className={`form-checkbox ${announcement.isResolved ? 'text-green-500' : 'text-red-500'}`}
+                                />
+                                <span className={`ml-2 ${announcement.isResolved ? 'text-green-600' : 'text-red-600'}`}>
+                                    {announcement.isResolved ? 'Решено' : 'Не решено'}
+                                </span>
+                            </label>
+                        ) : (
                             <span className={`ml-2 ${announcement.isResolved ? 'text-green-600' : 'text-red-600'}`}>
                                 {announcement.isResolved ? 'Решено' : 'Не решено'}
                             </span>
-                        </label>
+                        )}
                     </div>
                 ))}
             </div>
@@ -122,51 +129,6 @@ const ListAnnouncement = ({ userRole }) => {
                 <Modal isOpen={isEditModalOpen} onClose={closeEditModal}>
                     <EditAnnouncement id={currentAnnouncementId} onClose={closeEditModal} />
                 </Modal>
-            )}
-
-            {isViewAllModalOpen && ( // Модальное окно для показа всех объявлений
-                <Modal isOpen={isViewAllModalOpen} onClose={() => setIsViewAllModalOpen(false)}>
-                    <div className="bg-white rounded-lg p-6">
-                        <h2 className="text-xl font-semibold mb-4 text-center">Все объявления</h2>
-                        <div className="space-y-4">
-                            {announcements.map((announcement) => (
-                                <div key={announcement.id} className="border-b border-gray-200 pb-4 mb-4 relative">
-                                    <h3 className="text-lg font-semibold text-gray-900">{announcement.title}</h3>
-                                    <p className="text-gray-600 mt-2 whitespace-pre-line">{announcement.description}</p>
-                                    <p className="text-gray-400 text-sm mt-2">
-                                        {announcement.date.split('T')[0]}
-                                        &nbsp;
-                                        {announcement.date.slice('11', '19')}
-                                    </p>
-                                    {isAdmin && (
-                                        <div className="absolute top-0 right-0">
-                                            <button onClick={() => toggleMenu(announcement.id)}>
-                                                <FaEllipsisV className="text-gray-600 hover:text-gray-800" />
-                                            </button>
-                                            {activeMenu === announcement.id && (
-                                                <div className="absolute right-0 mt-2 py-2 w-48 bg-white border rounded shadow-xl">
-                                                    <button
-                                                        onClick={() => handleEdit(announcement.id)}
-                                                        className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-100"
-                                                    >
-                                                        Редактировать
-                                                    </button>
-                                                    <button
-                                                        onClick={() => handleDelete(announcement.id)}
-                                                        className="block w-full text-left px-4 py-2 text-red-700 hover:bg-red-100"
-                                                    >
-                                                        Удалить
-                                                    </button>
-                                                </div>
-                                            )}
-                                        </div>
-                                    )}
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </Modal>
-
             )}
         </div>
     );
